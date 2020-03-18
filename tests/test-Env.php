ed17d38e306812a612e614063ed931ca753b3c06
Boia Alexandru <alexboia_ro@yahoo.com>
2020-03-18 21:11:20 +0200

    Updated Abp01_Env tests for version 0.2.3

--- tests/test-Env.php
+++ tests/test-Env.php
@@ -68,7 +68,7 @@ class EnvTests extends WP_UnitTestCase {
 		$this->assertEquals(get_bloginfo('version', 'raw'), $env->getWpVersion());
 		$this->assertEquals('5.6.2', $env->getRequiredPhpVersion());
 		$this->assertEquals('5.0', $env->getRequiredWpVersion());
-		$this->assertEquals('0.2.2', $env->getVersion());
+		$this->assertEquals('0.2.3', $env->getVersion());
 	}
 
 	public function testCanGetDirectories() {
