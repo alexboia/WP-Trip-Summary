23b18d3834c36f94fac4c5cc911b1e49e497d378
Boia Alexandru <alexboia_ro@yahoo.com>
2020-05-08 12:49:46 +0300

    Further work on Compute and display altitude profile graph #57

--- media/js/abp01-altitude-profile.js
+++ media/js/abp01-altitude-profile.js
@@ -48,9 +48,16 @@
         options: {
             position: 'topleft',
             iconBaseUrl: null,
+            //Color used to plot the chart line
             chartLineColor: '#003ac1',
+            //Background color of the char tooltip
             tooltipBackgroundColor: 'rgba(81,81,81,0.80)',
+            //How much time has to pass since 
+            //  the last chart hover event before 
+            //  the map is adjusted to move the highlighted 
+            //  marker into vie
             hoverPanTimeout: 75,
+            //Labels used when constructing the chart tooltip
             labels: {
                 altitude: 'Altitude:',
                 distance: 'Distance:'
@@ -79,18 +86,26 @@
                 this._map.removeControl(this._currentPointMarker);
             }
 
+            //See if the marker is within the current bounds of the map
+            //  If not, start a timer to move the marker into view.
+            //  We're using a timer because, if the user 
+            //  shuflles rapidly between chart positions
+            //  we're going to have a lot of useless pans
             if (!this._map.getBounds().contains(markerCoord)) {
+                //First, cancel out the existing timer
                 if (this._currentHoverTimer != null) {
                     window.clearTimeout(this._currentHoverTimer);
                     this._currentHoverTimer = null;
                 }
     
+                //Start a new one
                 this._currentHoverTimer = window.setTimeout(function() {
                     me._currentHoverTimer = null;
                     me._map.panTo(markerCoord);
                 }, this.options.hoverPanTimeout);
             }
 
+            //Add the point anyway
             this._currentPointMarker = L.marker(markerCoord, {
                 icon: icon
             }).addTo(this._map);
@@ -126,16 +141,25 @@
         },
 
         _determineYAxisStepSize: function() {
-            var deltaAlt = Math.abs(this._profileInfo.maxAltitude - this._profileInfo.minAltitude);
+            var deltaAlt = Math.abs(this._profileInfo.maxAltitude.value
+                - this._profileInfo.minAltitude.value);
 
+            //If beween the minimum altitude and maximum altitude
+            //  the difference is greater than 1000 units
+            //  use a step size of 100
             if (deltaAlt >= 1000) {
                 return 100;
             } 
-
+            //If between 500 and 1000, use a step size of 70
+            if (deltaAlt > 500) {
+                return 70;
+            }
+            //If between 100 and 500, use a step size of 50
             if (deltaAlt > 100) {
                 return 50;
             }
             
+            //If below or at most 100, use a step size of 10
             return 10;
         },
 
@@ -218,21 +242,28 @@
         
         _hideProfileChart: function() {
             if (this._chart != null) {
+                //Remove the currently highlighted point from the map
                 this._removeCurrentHighlightedProfilePoint();
 
+                //Unbind event listeners
                 L.DomEvent.off(this._chartContainer, 
                     'mouseleave', 
                     this._handleChartAreaLeave, 
                     this);
 
+                //Destroy chart
                 this._chart.destroy();
                 this._chart = null;
 
+                //Empty chart container
                 L.DomUtil.empty(this._chartContainer);
             }
         },
 
         _handleChartAreaLeave: function() {
+            //When the mouse pointer leaves the chart area, 
+            //  remove the map marker that highlights 
+            //  the current point on the profile
             this._removeCurrentHighlightedProfilePoint();
         },
 
@@ -245,7 +276,7 @@
                 this._handleButtonClicked, 
                 this);
 
-            //add icon
+            //Add icon
             L.DomUtil.create('span', 
                 'dashicons dashicons-chart-area abp01-map-altitude-profile-btn', 
                 buttonLink);
@@ -257,6 +288,7 @@
         },
 
         _handleButtonClicked: function(event) {   
+            //Toggle chart on or off
             if (this._chart == null) {
                 this._showProfileChart();
             } else {
@@ -293,15 +325,18 @@
         },
 
         onRemove: function(map) {
+            //Unbind toggler event listeners
             L.DomEvent.off(this._buttonElement, 
                 'click', 
                 this._handleButtonClicked, 
                 this);
 
+            //Destroy chart if shown
             if (this._chart != null) {
                 this._hideProfileChart();
             }
 
+            //Cleanup the rest of stuff
             this._map = null;
             this._buttonElement = null;
         }
