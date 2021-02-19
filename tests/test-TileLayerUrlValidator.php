fd5c96a61a2c3d8476e14c378aa190ef7949fa60
Boia Alexandru <alexboia_ro@yahoo.com>
2021-02-19 23:13:01 +0200

    Refactored TileLayerUrlValidator tests.

--- tests/test-TileLayerUrlValidator.php
+++ tests/test-TileLayerUrlValidator.php
@@ -32,33 +32,55 @@
 class TileLayerUrlValidatorTests extends WP_UnitTestCase {
 	public function test_tryValidateEmptyUrl() {
 		$validator = new Abp01_Validate_TileLayerUrl();
-		$this->assertFalse($validator->validate(''));
-		$this->assertFalse($validator->validate(null));
-		$this->assertFalse($validator->validate(false));
+		foreach ($this->_getEmptyTileLayerUrls() as $emptyTileLayerUrl) {
+			$this->assertFalse($validator->validate($emptyTileLayerUrl));	
+		}
+	}
+
+	private function _getEmptyTileLayerUrls() {
+		return array(
+			'',
+			null,
+			false
+		);
 	}
 
 	public function test_canValidateUrl_validTileLayerUrl() {
 		$validator = new Abp01_Validate_TileLayerUrl();
-		$this->assertTrue($validator->validate('http://example.com/{z}/{x}/{y}'));
-		$this->assertTrue($validator->validate('http://{s}.example.com/{z}/{x}/{y}'));
-		$this->assertTrue($validator->validate('https://tiles.example.com/server/{z}/{x}/{y}'));
-		$this->assertTrue($validator->validate('https://{s}.tiles.example.com/server/{z}/{x}/{y}'));
-		
-		$this->assertTrue($validator->validate('ftp://example.com/{z}/{x}/{y}'));
-		$this->assertTrue($validator->validate('ftp://{s}.example.com/{z}/{x}/{y}'));
-		$this->assertTrue($validator->validate('ftps://tiles.example.com/server/{z}/{x}/{y}'));
-		$this->assertTrue($validator->validate('ftps://{s}.tiles.example.com/server/{z}/{x}/{y}'));
+		foreach ($this->_getValidTileLayerUrls() as $validTileLayerUrl)  {
+			$this->assertTrue($validator->validate($validTileLayerUrl));	
+		}
+	}
 
-		$this->assertTrue($validator->validate('http://{s}.tiles.example.com/{id}/server/{z}/{x}/{y}'));
-		$this->assertTrue($validator->validate('https://{s}.tiles.example.com/{id}/server/{z}/{x}/{y}'));
+	private function _getValidTileLayerUrls() {
+		return array(
+			'http://example.com/{z}/{x}/{y}',
+			'http://{s}.example.com/{z}/{x}/{y}',
+			'https://tiles.example.com/server/{z}/{x}/{y}',
+			'https://{s}.tiles.example.com/server/{z}/{x}/{y}',
+			'ftp://example.com/{z}/{x}/{y}',
+			'ftp://{s}.example.com/{z}/{x}/{y}',
+			'ftps://tiles.example.com/server/{z}/{x}/{y}',
+			'ftps://{s}.tiles.example.com/server/{z}/{x}/{y}',
+			'http://{s}.tiles.example.com/{id}/server/{z}/{x}/{y}',
+			'https://{s}.tiles.example.com/{id}/server/{z}/{x}/{y}'
+		);
 	}
 
 	public function test_canValidateUrl_invalidTileLayerUrl() {
 		$validator = new Abp01_Validate_TileLayerUrl();
-		$this->assertFalse($validator->validate('http://example.com/tiles/server'));
-		$this->assertFalse($validator->validate('http://example.com/tiles/server/{x}/{y}'));
-		$this->assertFalse($validator->validate('svn://{s}.tiles.example.com/server/{z}/{x}/{y}'));
-		$this->assertFalse($validator->validate('www.example.com/server/{z}/{x}/{y}'));
-		$this->assertFalse($validator->validate('bogus-url-server-{z}-{x}-{y}'));
+		foreach ($this->_getInvalidTileLayerUrls() as $invalidTileLayerUrl) {
+			$this->assertFalse($validator->validate($invalidTileLayerUrl));	
+		}
+	}
+
+	private function _getInvalidTileLayerUrls() {
+		return array(
+			'http://example.com/tiles/server',
+			'http://example.com/tiles/server/{x}/{y}',
+			'svn://{s}.tiles.example.com/server/{z}/{x}/{y}',
+			'www.example.com/server/{z}/{x}/{y}',
+			'bogus-url-server-{z}-{x}-{y}'			
+		);
 	}
 }
