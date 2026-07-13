ad299eb34c67c822c5ae118e647178c2b168dbe5
Alexandru Boia <alexandru.boia@outlook.com>
2026-07-13 15:48:14 +0300

    Remove vendor from source control

--- vendor/myclar/kamel-php/src/KmlParser/Entities/Folder.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types = 1);
-
-namespace KamelPhp\KmlParser\Entities;
-
-class Folder extends Container {
-	//
-}
--- vendor/myclar/kamel-php/src/XmlElement/Exceptions/UnexpectedXmlStructureException.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types = 1);
-
-namespace KamelPhp\XmlElement\Exceptions;
-
-class UnexpectedXmlStructureException extends \OutOfBoundsException {
-	//
-}
